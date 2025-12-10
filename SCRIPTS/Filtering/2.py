badc4bc7d8188ed73082cbb2d5dd927d3ca73220
Nima Taherkhani <n.taherkhani1378@gmail.com>
2025-12-10 18:21:22 +0100

    updated script + applying it on the Initial Right lead to better accruracy of targeted right

--- SCRIPTS/Filtering/2.py
+++ SCRIPTS/Filtering/2.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os 
 
-os.chdir(r"C:\Users\egzin\Desktop\UniPdfImp\thesis\DATA\RAW\notCategorized")
+os.chdir(r"C:\Users\egzin\Desktop\UniPdfImp\thesis\DATA\SCRIPTS\Filtering")
 print(os.getcwd())
 
 df = pd.read_csv('right_NC_Raw_V02.csv')
@@ -9,8 +9,8 @@ df.columns = ['A', 'B', 'C', 'D']
 
 ColD = df["D"]
 
-mask = ColD.str.startswith(("</u><u>", "</unc","now","away","there","after")).fillna(False)
+mask = ColD.str.startswith(("</u><u>", "</unc","now","away","there","after","onto","into")).fillna(False)
 
 print(ColD[mask].head(10).tolist())
 
-df[~mask].to_csv('right_NC_Raw_V08.csv', index=False)
+df[~mask].to_csv('right_Initial_Filtered_V09.csv', index=False)
